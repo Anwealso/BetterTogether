7531af79a50a485ab9960841db40d72d1ac58bff
Alon <alon.nusem@gmail.com>
2022-09-14 14:03:28 +1000

    Removed api app and fixed deprecated issues.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
     'myapi.apps.MyapiConfig',
     'rest_framework',
     'frontend.apps.FrontendConfig',

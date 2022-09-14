2128b2b050627375343e41c2b192fd9ec83559f5
jtopp0 <github@josephtopp.com>
2022-09-14 16:46:51 +1000

    added comments

--- mysite/frontend/src/components/App.js
+++ mysite/frontend/src/components/App.js
@@ -4,16 +4,16 @@ import { render } from "react-dom";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = { }; // ?
   }
 
   componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10') // placeholder api with sample json
          .then((response) => response.json())
          .then((data) => {
             console.log(data);
          })
-         .then((data) => this.setState({posts: data}))
+         .then((data) => this.setState({posts: data})) // Maybe this is wrong?
          .catch((err) => {
             console.log(err.message);
          });
@@ -22,7 +22,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="posts-container">
-        {posts.map((post) => {
+        {posts.map((post) => { // needs to be changed
           return (
             <div className="post-card" key={post.id}>
                <h2 className="post-title">{post.title}</h2>

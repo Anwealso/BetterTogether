b8581ac28532b671d146db846543e4ac95c1e758
jtopp0 <github@josephtopp.com>
2022-09-14 16:43:46 +1000

    Testing fetch

--- mysite/frontend/src/components/App.js
+++ mysite/frontend/src/components/App.js
@@ -4,10 +4,33 @@ import { render } from "react-dom";
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { };
+  }
+
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+         })
+         .then((data) => this.setState({posts: data}))
+         .catch((err) => {
+            console.log(err.message);
+         });
   }
 
   render() {
-    return <h1>Testing React Code</h1>;
+    return (
+      <div className="posts-container">
+        {posts.map((post) => {
+          return (
+            <div className="post-card" key={post.id}>
+               <h2 className="post-title">{post.title}</h2>
+            </div>
+         );
+      })}
+    </div>
+    );
   }
 }
 

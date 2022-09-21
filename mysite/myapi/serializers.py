c74300d79fcb7b6a7773761043b6b7e068bab8bf
Anwealso <48880195+Anwealso@users.noreply.github.com>
2022-09-21 12:56:48 +1000

    Added serializers for all basic classes

--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -8,3 +8,23 @@ from .models import Result
 #     class Meta:
 #         model = Hero
 #         fields = ('name', 'alias')
+
+class SurveySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survey
+        fields = ("name")
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ("text", "surveys")
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ("option", "question_id")
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = ("question_id", "choice_id", "survey_id", "sub_time")

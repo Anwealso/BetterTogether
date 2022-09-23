f461b0b4927e9fd8b600b59e8a7c4d8d05387021
Anwealso <48880195+Anwealso@users.noreply.github.com>
2022-09-23 16:27:16 +1000

    Added very basic rendering of survey page by getting survey data from backend api

--- mysite/frontend/src/components/Survey.js
+++ mysite/frontend/src/components/Survey.js
@@ -13,42 +13,41 @@ export default class Survey extends Component {
     // let params = useParams();
     // console.log(params)
 
-    // this.surveyId = this.props.match.params.surveyId;
-    // console.log(surveyId)
+    this.surveyId = this.props.match.params.surveyId;
     
-    // // this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
-    // // this.updateShowSettings = this.updateShowSettings.bind(this);
-    // this.renderSettingsButton = this.renderSettingsButton.bind(this);
-    // // this.renderSettings = this.renderSettings.bind(this);
-    // this.getSurveyDetails = this.getSurveyDetails.bind(this);
-    // this.getSurveyDetails();
+    // this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    // this.updateShowSettings = this.updateShowSettings.bind(this);
+    // this.renderSsettingsButton = this.renderSettingsButton.bind(this);
+    // this.renderSettings = this.renderSettings.bind(this);
+    this.getSurveyDetails = this.getSurveyDetails.bind(this);
+    this.getSurveyDetails();
   }
 
   render() {
-    console.log("Testing...")
+    // console.log("Testing...")
 
     return (<div>Hello, ""</div>);
   }
 
 
-  // getSurveyDetails() {
-  //   return fetch("/api/get-survey" + "?id=" + this.surveyId)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         console.log("1")
-  //         // this.props.leaveRoomCallback();
-  //         // this.props.history.push("/");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       this.setState({
-  //         name: data.name,
-  //         // guestCanPause: data.guest_can_pause,
-  //         // isHost: data.is_host,
-  //       });
-  //     });
-  // }
+  getSurveyDetails() {
+    return fetch("/api/get-survey" + "?id=" + this.surveyId)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Respone not okayyyyy")
+          // this.props.leaveRoomCallback();
+          // this.props.history.push("/");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          name: data.name,
+          // guestCanPause: data.guest_can_pause,
+          // isHost: data.is_host,
+        });
+      });
+  }
 
   // leaveButtonPressed() {
   //   const requestOptions = {
@@ -83,61 +82,61 @@ export default class Survey extends Component {
   //   );
   // }
 
-  // renderSettingsButton() {
-  //   return (
-  //     <Grid item xs={12} align="center">
-  //       <Button
-  //         variant="contained"
-  //         color="primary"
-  //         // onClick={() => this.updateShowSettings(true)}
-  //       >
-  //         Settings
-  //       </Button>
-  //     </Grid>
-  //   );
-  // }
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          // onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  }
 
-  // render() {
-  //   // if (this.state.showSettings) {
-  //   //   return this.renderSettings();
-  //   // }
-  //   return (
-  //     <Grid container spacing={1}>
+  render() {
+    // if (this.state.showSettings) {
+    //   return this.renderSettings();
+    // }
+    return (
+      <Grid container spacing={1}>
 
-  //       <Grid item xs={12} align="center">
-  //         <Typography variant="h4" component="h4">
-  //           Survey ID: {this.surveyId}
-  //         </Typography>
-  //       </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Survey ID: {this.surveyId}
+          </Typography>
+        </Grid>
 
-  //       {/* <Grid item xs={12} align="center">
-  //         <Typography variant="h6" component="h6">
-  //           Votes: {this.state.votesToSkip}
-  //         </Typography>
-  //       </Grid> */}
+        {/* <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid> */}
 
-  //       {/* <Grid item xs={12} align="center">
-  //         <Typography variant="h6" component="h6">
-  //           Guest Can Pause: {this.state.guestCanPause.toString()}
-  //         </Typography>
-  //       </Grid> */}
+        {/* <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid> */}
 
-  //       {/* <Grid item xs={12} align="center">
-  //         <Typography variant="h6" component="h6">
-  //           Host: {this.state.isHost.toString()}
-  //         </Typography>
-  //       </Grid> */}
+        {/* <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid> */}
 
-  //       {/* {this.state.isHost ? this.renderSettingsButton() : null} */}
-  //       {this.renderSettingsButton()}
+        {/* {this.state.isHost ? this.renderSettingsButton() : null} */}
+        {this.renderSettingsButton()}
 
-  //       {/* <Grid item xs={12} align="center">
-  //         <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
-  //           Leave Room
-  //         </Button>
-  //       </Grid> */}
+        {/* <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
+            Leave Room
+          </Button>
+        </Grid> */}
 
-  //     </Grid>
-  //   );
-  // }
+      </Grid>
+    );
+  }
 }

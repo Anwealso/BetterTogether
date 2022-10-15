3f56b2d83adc5a831a3a7ef6964644846605c0f6
4lon <alon.nusem@gmail.com>
2022-10-15 15:44:43 +1000

    Cleaned up events page and added users to event

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Survey(models.Model):
     name = models.CharField(max_length=60)
@@ -38,8 +39,8 @@ class Event(models.Model):
     location = models.CharField(max_length=100)
     time = models.DateTimeField()
     description = models.CharField(max_length=300)
-    image = models.CharField(max_length=100, null=True)
-    attendees = models.ManyToManyField(Survey, related_name='events')
+    image = models.CharField(max_length=200, null=True)
+    attendees = models.ManyToManyField(User, related_name='events', blank=True)
 
     def __str__(self):
         return self.title

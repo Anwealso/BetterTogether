63c1f27154ddfc22438e446a5aeecb32fca2feba
4lon <alon.nusem@gmail.com>
2022-10-15 15:44:43 +1000

    Cleaned up events page and added users to event

--- mysite/frontend/src/components/Events.js
+++ mysite/frontend/src/components/Events.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import { Routes, Route, useParams } from 'react-router-dom';
 import { Grid, Button, Typography, Radio, FormControl, FormControlLabel, FormLabel, RadioGroup , Card, CardContent, CardActions, CardMedia} from "@material-ui/core";
-// import { Grid, Button, Typography, Radio, FormControl, FormControlLabel, FormLabel, RadioGroup, Card } from "@material-ui/core";
-// import PosterImage from '../../static/images/ons_blanked.jpeg';
-// import * as React from 'react';
-// import Card, { Grid } from '@material-ui/core';
-// import CardActions from '@material-ui/core';
-// import CardContent from '@material-ui/core';
-// import CardMedia from '@material-ui/core';
-// import Button from '@material-ui/core';
-// import Typography from '@material-ui/core';
+import Navbar from "./Navbar";
 
 export default class Events extends Component {
   constructor(props) {
@@ -43,8 +35,6 @@ export default class Events extends Component {
           image={props.image}
         />
         <CardContent>
-          {/* <Typography gutterBottom variant="h5" component="div">
-          </Typography> */}
           <Typography gutterBottom variant="h5" component="div">
             {props.title}
           </Typography>
@@ -85,88 +75,23 @@ export default class Events extends Component {
   }
 
 
-  // todo: add css and divs
   render() {
     return (
-      <div className="posts-container">
-        <h2> Event List</h2>
-         {this.state.events.map((event) => {
-          // import * as React from 'react';
-          // import Card from '@mui/material/Card';
-          // import CardActions from '@mui/material/CardActions';
-          // import CardContent from '@mui/material/CardContent';
-          // import CardMedia from '@mui/material/CardMedia';
-          // import Button from '@mui/material/Button';
-          // import Typography from '@mui/material/Typography';
-          
-            return (
-              
-            //   <Card sx={{ maxWidth: 345 }}>
-            //     <CardMedia
-            //       component="img"
-            //       height="140"
-            //       image="/static/images/cards/contemplative-reptile.jpg"
-            //       alt="green iguana"
-            //     />
-            //     <CardContent>
-            //       <Typography gutterBottom variant="h5" component="div">
-            //         Lizard
-            //       </Typography>
-            //       <Typography variant="body2" color="text.secondary">
-            //         Lizards are a widespread group of squamate reptiles, with over 6,000
-            //         species, ranging across all continents except Antarctica
-            //       </Typography>
-            //     </CardContent>
-            //     <CardActions>
-            //       <Button size="small">Share</Button>
-            //       <Button size="small">Learn More</Button>
-            //     </CardActions>
-            //   </Card>
-            // );
-          
-            // return (
-              
-            //       <Card sx={{ maxWidth: 345 }} key={event.id}>
-            //         {/* <CardMedia
-            //           component="img"
-            //           height="140"
-            //           // image="/static/images/cards/contemplative-reptile.jpg"
-            //           alt="green iguana"
-            //         /> */}
-            //         {/* <CardContent>
-            //           <Typography gutterBottom variant="h5" component="div">
-            //             Lizard
-            //           </Typography>
-            //           <Typography variant="body2" color="text.secondary">
-            //             Lizards are a widespread group of squamate reptiles, with over 6,000
-            //             species, ranging across all continents except Antarctica
-            //           </Typography>
-            //         </CardContent>
-            //         <CardActions>
-            //           <Button size="small">Share</Button>
-            //           <Button size="small">Learn More</Button>
-            //         </CardActions> */}
-            //       </Card>
-              
-              <Grid>
-                {this.renderMedia(event)}
-               {/* <div className="post-card" key={event.id}>
 
-                  <h2 className="post-title">{event.title}</h2>
-                  <p className="post-body">{event.location}</p>
-                  <p className="post-body">{event.time}</p>
-                  <p className="post-body">{event.description}</p>
-               </div> */}
-               </Grid>
-            );
-         })}
-      </div>
+      <Grid container spacing={1}>
+          <Navbar />
+
+          <Grid item xs={12} align="center">
+
+                {this.state.events.map((event) => {
+                    return (      
+                      <Grid style={{backgroundColor: "ghostwhite", borderRadius: "20px", margin: "10px", padding: "10px", width: "50%"}}>
+                        {this.renderMedia(event)}
+                       </Grid>
+                    );
+                 })}
+          </Grid>
+        </Grid>
       );
   }
 }
-
-// import * as React from 'react';
-// // import { Grid, Button, Typography, Radio, FormControl, FormControlLabel, FormLabel, RadioGroup, Card } from "@material-ui/core";
-// import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-
-

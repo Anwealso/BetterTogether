c8d1cbcf6b70f65c98e78736e0178edaea134caf
4lon <alon.nusem@gmail.com>
2022-10-17 15:56:12 +1000

    FIX BREAKING ERROR

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -44,16 +44,6 @@ class Group(models.Model):
         return self.title
 
 
-class Group(models.Model):
-    title = models.CharField(max_length=60)
-    location = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
-    image = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return self.title
-
-
 class Event(models.Model):
     title = models.CharField(max_length=60)
     location = models.CharField(max_length=100)
